85aa36f7730105400406f9a21f2ebaf1cd96d792
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-01-17 21:57:03 +0100

    finish game need to comment text

--- main.js
+++ main.js
@@ -1,36 +1,129 @@
-var card = document.getElementsByClassName("card");
-var catImg = [];
+const catsArray = [{
+        'name' : 'fenris',
+        'img' : 'img/fenris.jpg'
+    },
+    {
+        'name' : 'findus',
+        'img' : 'img/findus.jpg'
+    },
+    {
+        'name' : 'lotus',
+        'img' : 'img/lotus.jpg'
+    },
+    {
+        'name' : 'luvan',
+        'img' : 'img/luvan.jpg'
+    },
+    {
+        'name' : 'norna',
+        'img' : 'img/norna.jpg'
+    },
+    {
+        'name' : 'rocky',
+        'img' : 'img/rocky.jpg'
+    },
+    {
+        'name' : 'rut',
+        'img' : 'img/rut.jpg'
+    },
+    {
+        'name' : 'samus',
+        'img' : 'img/samus.jpg'
+    },
+    {
+        'name' : 'tiger_02',
+        'img' : 'img/tiger_02.jpg'
+    },
+    {
+        'name' : 'zefyrus',
+        'img' : 'img/zefyrus.jpg'
+    }
+];
+const gameGrid = catsArray
+  .concat(catsArray)
+  .sort(() => 0.5 - Math.random());
 
-// Loops through all the images and adds event listener to each image.
-for (var i = 0; i < catImg.length; i++) {
-    catImg[i].addEventListener("click", displayCard)
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
+
+const game = document.getElementById('game');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+gameGrid.forEach(item => {
+  const { name, img } = item;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${img})`;
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.add('match');
+  });
 };
 
-// The Displayed cards get a function which toggles (Open, show and disabled) cards.
-var displayCard = function () {
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
-}
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
 
-
-
-
-
-/*
-
-buildHTML: function(){
-    var frag = '';
-    this.$cards.each(function(k, v){
-        frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
-        <div class="front"><img src="'+ v.img +'"\
-        alt="'+ v.name +'" /></div>\
-        <div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
-        alt="Codepen" /></div></div>\
-        </div>';
-    });
-    return frag;
-}
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
 };
 
-*/
+grid.addEventListener('click', event => {
+
+  const clicked = event.target;
+
+  if (
+    clicked.nodeName === 'SECTION' ||
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains('selected') ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+
+});

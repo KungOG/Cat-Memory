1dc3a5177a99d1711a916f3e598884be01de3b32
Åsa Palholmen <asa@palholmen.se>
2019-01-17 15:26:20 +0100

    trying to make a memoryboard

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
 var card = document.getElementsByClassName("card");
 var catImg = [];
+var drawCards = [];
 
 // Loops through all the images and adds event listener to each image.
 for (var i = 0; i < catImg.length; i++) {
-    catImg[i].addEventListener("click", displayCard)
+  catImg[i].addEventListener("click", displayCard)
 };
 
 // The Displayed cards get a function which toggles (Open, show and disabled) cards.
-var displayCard = function () {
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
+var displayCard = function() {
+  this.classList.toggle("open");
+  this.classList.toggle("show");
+  this.classList.toggle("disabled");
 }
 
+function drawMemory(drawCards) {
+  drawCards = [];
+  for (let i = 1; i < 6; i++) {
+    var htmlCards = `<div class="card" id="${i}"><div class="inside"><div class="front"><img src="'1x1/${i}.jpg'"></div>
+        <div class="back"><img src="1x1/baksida.jpg"\ alt="?"></div></div>
+        </div>`;
+    drawCards.push(htmlCards);
+    console.log(drawCards);
+  };
+  drawHtmlMemory();
+    console.log(drawCards);
+  return drawCards;
 
-
-
-
-/*
-
-buildHTML: function(){
-    var frag = '';
-    this.$cards.each(function(k, v){
-        frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
-        <div class="front"><img src="'+ v.img +'"\
-        alt="'+ v.name +'" /></div>\
-        <div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
-        alt="Codepen" /></div></div>\
-        </div>';
-    });
-    return frag;
-}
 };
 
-*/
+function drawHtmlMemory(drawCards) {
+  //get memory and save it in memorydiv then
+  //write out drawCards in memorydiv
+  console.log(drawCards);
+  document.getElementById("memory").innerHTML = drawCards;
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+  drawMemory();
+});

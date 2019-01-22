ab33d1a8d8079f3d58c7bc4995f50a5ee201ebed
Åsa Palholmen <asa@palholmen.se>
2019-01-22 13:34:01 +0100

    fixed mergedproblem

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let secondGuess = '';
 let count = 0;
 let previousTarget = null;
 let delay = 1200;
+var userHasClicked = false;
 
 let steps = document.querySelector('.steps');
 let matchCount = 0;
@@ -19,6 +20,10 @@ const reload = document.getElementById('reload');
 
 const game = document.getElementById('game');
 const grid = document.createElement('section');
+
+var second = 0, minute = 0;
+var timer = document.querySelector('.timer');
+var interval = '';
 
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
@@ -73,7 +78,10 @@ const resetGuesses = () => {
 
 /* Checks if the clicks have a match or not */
 grid.addEventListener('click', event => {
-
+  if (!userHasClicked) {
+          startTimer();
+          userHasClicked = true;
+      }
   const clicked = event.target;
 
   if (
@@ -117,9 +125,7 @@ function closeModal () {
     })
 }
 
-var second = 0, minute = 0;
-var timer = document.querySelector('.timer');
-var interval;
+
 function startTimer() {
     interval = setInterval(function() {
         timer.innerHTML = minute+"mins "+second+"secs";

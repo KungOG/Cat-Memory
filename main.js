3772b83347da07e11e384e1c1c361c185f4f7d67
Åsa Palholmen <asa@palholmen.se>
2019-01-22 11:03:23 +0100

    put the cats in another file

--- main.js
+++ main.js
@@ -1,52 +1,5 @@
-const catsArray = [{
-        'name' : 'fenris',
-        'img' : 'img/fenris_y.jpg'
-    },
-    {
-        'name' : 'findus',
-        'img' : 'img/findus_g.jpg'
-    },
-    {
-        'name' : 'lotus',
-        'img' : 'img/lotus_g.jpg'
-    },
-    {
-        'name' : 'luvan',
-        'img' : 'img/luvan_p.jpg'
-    },
-    {
-        'name' : 'norna',
-        'img' : 'img/norna_o.jpg'
-    },
-    {
-        'name' : 'corona',
-        'img' : 'img/corona_b.jpg'
-    },
-    {
-        'name' : 'rut',
-        'img' : 'img/rut_r.jpg'
-    },
-    {
-        'name' : 'samus',
-        'img' : 'img/samus_y.jpg'
-    },
-    {
-        'name' : 'tiger',
-        'img' : 'img/tiger_b.jpg'
-    },
-    {
-        'name' : 'zephyrus',
-        'img' : 'img/zephyrus_b.jpg'
-    },
-    {
-        'name' : 'percy',
-        'img' : 'img/percy_t.jpg'
-    },
-    {
-        'name' : 'octarine',
-        'img' : 'img/octarine_b.jpg'
-    }
-];
+import cats from './cats.js'
+
 const gameGrid = catsArray
   .concat(catsArray)
   .sort(() => 0.5 - Math.random());
